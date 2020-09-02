064085ea807e76b02375b41dcb1872262971987a
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-09-02 16:19:30 -0700

    All events now happening in order

--- socket-io/vendor.js
+++ socket-io/vendor.js
@@ -4,11 +4,27 @@ require('dotenv').config();
 
 const io = require('socket.io-client');
 const storeName = process.env.STORE_NAME;
+const faker = require('faker');
 
-const capsRoom = io.connect(`http://localhost:${process.env.PORT}/caps`);
+const capsNamespace = io.connect(`http://localhost:${process.env.PORT || 8080}/caps`);
 
+capsNamespace.emit('join', storeName);
+capsNamespace.on('delivered', payload => {
+  console.log(`Thank you for delivering ${payload.orderID}`);
+});
 
-capsRoom.emit('join', storeName);
+function startOrders() {
+  setInterval(() => {
+    const fakeOrder = {
+      store: storeName,
+      orderID: faker.random.alphaNumeric(20),
+      customer: faker.fake('{{name.firstName}} {{name.lastName}}'),
+      address: faker.address.streetAddress(),
+    };
+    capsNamespace.emit('pickup', fakeOrder);
+  }, 5000);
+}
 
+startOrders();
 
 
