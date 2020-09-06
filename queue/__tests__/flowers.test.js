8304d93c4f9128abe6704ec167aa84683c9fc564
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-09-05 21:37:44 -0700

    tests now running

---
+++ queue/__tests__/flowers.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('../1-206-flowers');
+const client = require('socket.io-client');
+const { internet } = require('faker');
+let socket = client.connect();
+
+
+describe('tests for socket queue', () => {
+  it('sends received delivery message', () => {
+    
+    console.log = jest.fn();
+
+    socket.emit('delivered', {orderID: '1234'});
+
+    expect(console.log).toHaveBeenCalledWith('Thank you for delivering 1234');
+   
+  });
+
+  it('emits the received handler on delivery', () => {
+
+    const receivedHandler = jest.fn();
+
+    socket.on('received', receivedHandler);
+
+    socket.emit('delivered', {orderID: '1234'});
+
+    expect(receivedHandler).toHaveBeenCalled();
+
+  });
+});
+
+
